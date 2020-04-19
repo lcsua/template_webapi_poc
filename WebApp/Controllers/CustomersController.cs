495f8da4cb853741b9593cf252da7457837cea84
lcsua <lucasmbolivar@gmail.com>
2020-04-18 21:51:45 -0300

    new customer controller

---
+++ WebApp/Controllers/CustomersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Core.Aplication.Interfaces;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomersController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+        public CustomersController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+
+       
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return  Ok(_customerRepository.GetCustomers());
+        }
+
+        [HttpGet("{customerId}")]
+        public IActionResult Index(int customerId)
+        {
+            return Ok(_customerRepository.GetCustomerByID(customerId));
+        }
+    }
+}
