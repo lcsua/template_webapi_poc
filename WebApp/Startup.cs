b2b28e6239c36be8dc8617bced8064c5db52433f
lcsua <lucasmbolivar@gmail.com>
2020-04-18 21:52:20 -0300

    wire-up

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebApp.Core.Aplication.Interfaces;
+using WebApp.Infrastructure.Repositories;
 
 namespace WebApp
 {
@@ -26,6 +28,7 @@ namespace WebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSingleton<ICustomerRepository, CustomerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
